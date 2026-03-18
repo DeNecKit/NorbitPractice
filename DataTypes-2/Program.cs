fce35480d9cf1db28ff63970422aa329c0bbc18f
DeNecKit <deneckitc.2004@gmail.com>
2026-03-18 17:48:47 +0300

    [DataTypes-2] Добавить решение на задание 2.

---
+++ DataTypes-2/Program.cs
@@ -0,0 +1,52 @@
+﻿/// <summary>
+/// Выводит на экран ромб из символов X с диагоналями длиной n
+/// </summary>
+/// <param name="n">Положительная нечётная длина диагоналей</param>
+void PrintDiamond(int n)
+{
+    if ((n <= 0) || (n % 2 == 0))
+        throw new ArgumentException("n must be a positive odd number");
+
+    int space = n / 2 - 1;
+    int x = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        if ((i == 0) || (i == n - 1))
+        {
+            for (int j = 0; j < n / 2; j++) Console.Write(" ");
+            Console.Write("X");
+            for (int j = 0; j < n / 2; j++) Console.Write(" ");
+        }
+        else
+        {
+            for (int j = 0; j < space; j++) Console.Write(" ");
+            Console.Write("X");
+            for (int j = 0; j < x; j++) Console.Write(" ");
+            Console.Write("X");
+            for (int j = 0; j < space; j++) Console.Write(" ");
+
+            if (i < n / 2)
+            {
+                space--;
+                x += 2;
+            }
+            else
+            {
+                space++;
+                x -= 2;
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
+
+try
+{
+    PrintDiamond(5);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
