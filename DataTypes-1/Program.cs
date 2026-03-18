d190e650f0466e987c1ba31f37be7c16bc9ca32a
DeNecKit <deneckitc.2004@gmail.com>
2026-03-18 16:59:52 +0300

    Добавьте файлы проекта.

---
+++ DataTypes-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+string CalculateDeposit(double initialDeposit, int years, double interestRate)
+{
+    if (initialDeposit <= 0)
+        throw new ArgumentException("initialDeposit must be positive");
+    if (years <= 0)
+        throw new ArgumentException("years must be positive");
+    if (interestRate <= 0)
+        throw new ArgumentException("interestRate must be positive");
+
+    StringBuilder result = new();
+    double sum = initialDeposit;
+
+    for (int year = 1; year <= years; year++)
+    {
+        sum += sum * (interestRate / 100.0);
+        result.AppendLine($"Год {year}: {sum:0.00} руб.");
+    }
+
+    return result.ToString();
+}
+
+Console.WriteLine(CalculateDeposit(1000.0, 3, 10.0));
