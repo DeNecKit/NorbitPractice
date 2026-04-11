dfc9795e8d46e7fd7c6438cb70776e093874e8c3
DeNecKit <deneckitc.2004@gmail.com>
2026-04-11 14:47:44 +0300

    [Cinema] Удалить неиспользуемые методы CRUD.

--- CinemaApp/Controllers/TicketsController.cs
+++ CinemaApp/Controllers/TicketsController.cs
@@ -39,35 +39,4 @@ public class TicketsController(ApplicationContext context) : ControllerBase
 
         return CreatedAtAction(nameof(Get), new { id = item.Id }, item);
     }
-
-    [HttpPut("{id}")]
-    public async Task<ActionResult> Put(int id, Ticket item)
-    {
-        if (id != item.Id) return BadRequest("Id mismatch");
-
-        try
-        {
-            _context.Entry(item).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
-        }
-        catch (DbUpdateConcurrencyException)
-        {
-            if (await _context.Tickets.FindAsync(id) is null) return NotFound();
-            throw;
-        }
-
-        return NoContent();
-    }
-
-    [HttpDelete("{id}")]
-    public async Task<ActionResult> Delete(int id)
-    {
-        Ticket? ticket = await _context.Tickets.FindAsync(id);
-        if (ticket is null) return NotFound();
-
-        _context.Tickets.Remove(ticket);
-        await _context.SaveChangesAsync();
-
-        return NoContent();
-    }
 }
