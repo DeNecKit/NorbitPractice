c753947940761c3649fe8bda5f1d9c01ceb4f1c0
DeNecKit <deneckitc.2004@gmail.com>
2026-04-13 10:04:01 +0300

    [Cinema] Мигрирование БД.

---
+++ CinemaApp/Migrations/20260413065528_UpdateTicketAddGuidAndDto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTicketAddGuidAndDto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tickets_SessionId",
+                table: "Tickets");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "PublicId",
+                table: "Tickets",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_PublicId",
+                table: "Tickets",
+                column: "PublicId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_SessionId_Row_Seat",
+                table: "Tickets",
+                columns: new[] { "SessionId", "Row", "Seat" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tickets_PublicId",
+                table: "Tickets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tickets_SessionId_Row_Seat",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "Tickets");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_SessionId",
+                table: "Tickets",
+                column: "SessionId");
+        }
+    }
+}
